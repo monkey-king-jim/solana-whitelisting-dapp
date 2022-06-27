74a2d061a45f571ba476f1e12ccf5a8efa5be2e6
monkey-king-jim <jameswwei@hotmail.com>
2022-06-26 22:55:50 -0400

    add create_whitelist

--- programs/solana-whitelisting-dapp/src/lib.rs
+++ programs/solana-whitelisting-dapp/src/lib.rs
@@ -1,15 +1,49 @@
 use anchor_lang::prelude::*;
+// use anchor_lang::solana_program::account_info::AccountInfo;
+// use anchor_spl::token::{self, Token, Mint, TokenAccount, Transfer};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8UCFsbJjuTzUimm4g9TuVooR3dKEC7MNV8wyqZp8TEKH");
 
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBKEY_LENGTH: usize = 32;
+// const UNSIGNED64_LENGTH: usize = 8;
+
+// business logic
 #[program]
-pub mod solana_whitelisting_dapp {
+pub mod whitelisting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_whitelist(ctx: Context<CreateWhitelist>, added_address: u8) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.authority = ctx.accounts.user.key();
+        counter_account.count = 0;
         Ok(())
     }
 }
 
+// data validators
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(added_address: u8)]
+pub struct CreateWhitelist<'info> {
+    #[account(
+        init, 
+        payer = user, 
+        space = Whitelist::LEN, 
+        seeds = [b"test", user.key().as_ref(), &[added_address]], 
+        bump
+    )]
+    pub whitelist: Account<'info, Whitelist>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// data structures
+#[account]
+pub struct Whitelist {
+}
+
+
+impl Whitelist {
+    const LEN: usize = DISCRIMINATOR_LENGTH + PUBKEY_LENGTH;
+}
