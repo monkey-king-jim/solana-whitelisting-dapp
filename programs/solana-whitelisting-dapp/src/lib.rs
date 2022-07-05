7724d2b4611aeb3b0b8304dc4834649f116cb67c
monkey-king-jim <jameswwei@hotmail.com>
2022-07-05 18:38:34 -0400

    create addToWhitelist fn

--- programs/solana-whitelisting-dapp/src/lib.rs
+++ programs/solana-whitelisting-dapp/src/lib.rs
@@ -2,45 +2,83 @@ use anchor_lang::prelude::*;
 // use anchor_lang::solana_program::account_info::AccountInfo;
 // use anchor_spl::token::{self, Token, Mint, TokenAccount, Transfer};
 
-declare_id!("8UCFsbJjuTzUimm4g9TuVooR3dKEC7MNV8wyqZp8TEKH");
+declare_id!("GgcfkCKzrDHHnuhfAEvC7ohNjZk6hY9KZW2t9Sk7neRX");
 
 const DISCRIMINATOR_LENGTH: usize = 8;
 const PUBKEY_LENGTH: usize = 32;
 // const UNSIGNED64_LENGTH: usize = 8;
 
-// business logic
+// business logics
 #[program]
 pub mod whitelisting {
     use super::*;
 
     pub fn create_whitelist(ctx: Context<CreateWhitelist>) -> Result<()> {
+        let whitelist = &mut ctx.accounts.whitelist;
+        whitelist.authority = ctx.accounts.authority.key();
+        Ok(())
+    }
+
+    pub fn add_to_whitelist(ctx: Context<AddToWhitelist>) -> Result<()> {
         Ok(())
     }
 }
 
 // data validators
 #[derive(Accounts)]
-#[instruction(added_address: u8)]
 pub struct CreateWhitelist<'info> {
     #[account(
         init, 
-        payer = user, 
+        payer = authority, 
         space = Whitelist::LEN, 
-        seeds = [b"test", user.key().as_ref(), &[added_address]], 
+        seeds = [
+            b"test", 
+            authority.key().as_ref(), 
+        ],
         bump
     )]
     pub whitelist: Account<'info, Whitelist>,
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddToWhitelist<'info> {
+    #[account(mut, has_one=authority)]
+    pub whitelist: Account<'info, Whitelist>,
+    pub wallet: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        init,
+        payer = authority,
+        space = WhitelistData::LEN,
+        seeds = [
+            whitelist.key().as_ref(),
+            wallet.key().as_ref(),
+        ],
+        bump
+    )]
+    pub whitelist_data: Account<'info, WhitelistData>,
     pub system_program: Program<'info, System>,
 }
 
 // data structures
 #[account]
 pub struct Whitelist {
+    pub authority: Pubkey
 }
 
 
 impl Whitelist {
     const LEN: usize = DISCRIMINATOR_LENGTH + PUBKEY_LENGTH;
+}
+
+#[account]
+pub struct WhitelistData {
+}
+
+impl WhitelistData {
+    const LEN: usize = DISCRIMINATOR_LENGTH;
 }
--- tests/solana-whitelisting-dapp.ts
+++ tests/solana-whitelisting-dapp.ts
@@ -1,16 +1,40 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { SolanaWhitelistingDapp } from "../target/types/solana_whitelisting_dapp";
+import { Whitelisting } from "../target/types/whitelisting";
+import { Keypair, SystemProgram } from "@solana/web3.js";
 
 describe("solana-whitelisting-dapp", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.SolanaWhitelistingDapp as Program<SolanaWhitelistingDapp>;
+  const program = anchor.workspace
+    .SolanaWhitelistingDapp as Program<Whitelisting>;
+  console.log(anchor.workspace);
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+    const wallet = provider.wallet;
+    const systemProgram = anchor.web3.SystemProgram;
+    const program = anchor.workspace.Whitelisting as Program<Whitelisting>;
+
+    const added_address = Keypair.generate();
+    const added_address_pubkey = added_address.publicKey;
+
+    const [whitelist, _whitelistBump] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [anchor.utils.bytes.utf8.encode("test"), wallet.publicKey.toBytes()],
+        program.programId
+      );
+
+    const tx = await program.methods
+      .createWhitelist("foo")
+      .accounts({
+        user: wallet.publicKey,
+        whitelist,
+        systemProgram: systemProgram.programId,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
